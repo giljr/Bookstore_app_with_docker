e1a184b5f3a6467e896aa32417975ec837e9cb3b
jaythree <giljr.2009@gmail.com>
2025-02-21 12:17:27 +0000

    Initial commit for episode-1-tailwind-and-devise

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+
+        if @book.save
+            respond_to do |format|
+                format.turbo_stream
+            end
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def book_params
+        params.expect(book: [ :name, :price ])
+    end
+end
